f7a34ed21af43f14c472501b525b05f3a3a65295
Oscarprezz <oscar.ramadan.perez@gmail.com>
2020-03-06 18:21:47 +0100

    Actualizacion de Oscar

--- PruebaMP/src/pruebamp/PruebaMP.java
+++ PruebaMP/src/pruebamp/PruebaMP.java
@@ -3,7 +3,7 @@ package pruebamp;
 public class PruebaMP {
 
     public static void main(String[] args) {
-        System.out.print("HOLA MUNDO/HELLO WORLD!!!!!!!!!!!!!!!!!!");
+        System.out.print("HOLA MUNDO/HELLO WORLD Oscar es un cagon!!!!!!!!!!!!!!!!!!");
     }
     
 }
